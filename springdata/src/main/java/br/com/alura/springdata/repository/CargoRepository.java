932e348604b507812388c82e2be0865d93f2b796
Chrystian <chrystianfarias@hotmail.com>
2023-03-15 11:13:57 -0400

    Inserindo Valores

--- springdata/src/main/java/br/com/alura/springdata/repository/CargoRepository.java
+++ springdata/src/main/java/br/com/alura/springdata/repository/CargoRepository.java
@@ -1,5 +1,12 @@
 package br.com.alura.springdata.repository;
 
-public interface CargoRepository {
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.alura.springdata.orm.Cargo;
+
+@Repository
+public interface CargoRepository extends CrudRepository<Cargo, Integer>{
 
 }
+ 
